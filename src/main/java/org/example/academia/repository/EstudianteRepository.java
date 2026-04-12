1cc5ec10f35c9648a4d0e2d92a8a7a17b450de7e
mike7019 <amph7019@gmail.com>
2026-04-12 00:36:28 -0500

    Revert "Merge pull request #4 from mike7019/elkin"
    This reverts commit 18d6b7523d4b1dbf8e1e5a8770c4deb59f08ea6e, reversing
    changes made to b463a7e63eebc4e8729dfcde4787ffefea868900.

--- src/main/java/org/example/academia/repository/EstudianteRepository.java
+++ src/main/java/org/example/academia/repository/EstudianteRepository.java
@@ -2,12 +2,7 @@ package org.example.academia.repository;
 
 import org.example.academia.domain.entity.Estudiante;
 
-import java.time.LocalDate;
-
 public interface EstudianteRepository {
 
-    long countByActivoTrue();
-
-    long countByFechaRegistroBetween(LocalDate start, LocalDate end);
-
 }
+
